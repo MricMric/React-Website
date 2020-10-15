84f46b6ad5c1fc9b9492a1b4ec8982ebc3e53c2a
Aymeric D <aymericmetms@hotmail.fr>
2020-10-15 14:06:55 +0200

    Ajout theme Dark

--- site-cv-react/src/App.js
+++ site-cv-react/src/App.js
@@ -4,14 +4,16 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import { Header, Main, Heading, Paragraph,Button, Sidebar, Nav, Menu, Avatar, Box, Button as GrommetButton, Grommet } from 'grommet';
 import { Home, Projects, Clock, Help} from 'grommet-icons';
+import { hpe } from 'grommet-theme-hpe';
 
 function App() {
   return (
-    <Main pad="large">
-  <Heading>Something</Heading>
-  <Paragraph>Something about something</Paragraph>
+    <Grommet theme={hpe} themeMode="dark">
   
-</Main>
+  <Box pad="large" background="doc" />
+</Grommet>
+
+    
   );
 }
 
