e6586ff3940ed51b7d584f26d0578ab51aa032f7
Aymeric D <aymericmetms@hotmail.fr>
2020-10-15 13:37:47 +0200

    Ajout grommet

--- site-cv-react/src/App.js
+++ site-cv-react/src/App.js
@@ -2,56 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Header, Main, Heading, Paragraph,Button, Sidebar, Nav, Menu, Avatar, Box, Button as GrommetButton, Grommet } from 'grommet';
+import { Home, Projects, Clock, Help} from 'grommet-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Main pad="large">
+  <Heading>Something</Heading>
+  <Paragraph>Something about something</Paragraph>
+  
+</Main>
   );
 }
 
