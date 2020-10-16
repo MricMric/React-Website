95922e6fdf753ac2496bfdb3c1e9febb07621c66
Aymeric D <aymericmetms@hotmail.fr>
2020-10-16 11:20:12 +0200

    Ajout reponsivité

--- site-cv-react/src/App.js
+++ site-cv-react/src/App.js
@@ -37,6 +37,7 @@ function App() {
 
   <Grommet theme={hpe}>
     <HeaderNav/>
+    <TopMain/>
     <GridMain/>
     <FooterBot/>
   </Grommet>
@@ -54,143 +55,257 @@ const customTheme = deepMerge(grommet, {
 
 function HeaderNav() {
   return (
+    <>
     <Header background="dark-1" pad="medium">
-    <Box direction="row"  gap="small">
-    </Box>
-    <Box direction="row" align="center" gap="medium">
-      Voici mon Menu 
-    </Box>
-    <ResponsiveContext.Consumer>
-      {responsive =>
-        responsive === 'small' ? (
-          <Menu
-            label="More"
-            items={[
-              { label: 'This is', onClick: () => {} },
-              { label: 'The Menu', onClick: () => {} },
-              { label: 'Component', onClick: () => {} },
-            ]}
-          />
-        ) : (
-          <Nav direction="row">
-            <Anchor href="#" label="This is" />
-            <Anchor href="#" label="The Nav" />
-            <Anchor href="#" label="Component" />
-            <Anchor icon={<Home />} hoverIndicator />
-            <Anchor icon={<Facebook />} hoverIndicator />
-            <Anchor icon={<ChatOption />} hoverIndicator />
-          </Nav>
-        )
-      }
-    </ResponsiveContext.Consumer>
-  </Header>
+      <Box direction="row" align="center" gap="small">
+        Resize the page to collapse the Nav into a Menu
+      </Box>
+      <ResponsiveContext.Consumer>
+        {responsive =>
+          responsive === 'small' ? (
+            <Menu
+              label="Click me"
+              items={[
+                { label: 'This is', onClick: () => {} },
+                { label: 'The Menu', onClick: () => {} },
+                { label: 'Component', onClick: () => {} },
+              ]}
+            />
+          ) : (
+            <Nav direction="row">
+              <Anchor href="#" label="This is" />
+              <Anchor href="#" label="The Nav" />
+              <Anchor href="#" label="Component" />
+            </Nav>
+          )
+        }
+      </ResponsiveContext.Consumer>
+    </Header>
+  </>
     );
 }
 
-function GridMain() {
+function TopMain() {
+    const size = React.useContext(ResponsiveContext);
   return (
     <>
-    <Grommet theme={customTheme}>
-    <Grid
-      fill
-      areas={[
-        { name: 'nav', start: [0, 0], end: [0, 0] },
-        { name: 'main', start: [1, 0], end: [1, 0] },
-      ]}
-      columns={['medium', 'flex']}
-      rows={['flex']}
-      gap="large"
-    >
-      <Box gridArea="nav" margin="large">
-        <Card width="medium" height="medium">
-              <Stack anchor="bottom-left">
-                <CardBody height="medium">
-                  <Image
-                    fit="cover"
-                    src={moi}
-                    a11yTitle="aymeric"
-                  />
-                </CardBody>
-                <CardHeader
-                  pad={{ horizontal: 'small', vertical: 'small' }}
-                  background="#000000A0"
-                  width="medium"
-                  justify="start"
-                >
-                  <Avatar a11yTitle="avatar"></Avatar>
-                  <Box>
-                    <Heading level="3" margin="none">
-                      Aymeric
-                    </Heading>
-                    <Text size="small" textAlign="center" weight="bold">DELABARRE</Text>
-                  </Box>
-                </CardHeader>
-              </Stack>
-            </Card>
-      </Box>
-      <Box gridArea="main" margin="large">
-        <Paragraph>
-          <Text size='22px' weight="bold">B</Text>onjour, je m'appelle Aymeric, j'ai 21 ans et je suis 
-            actuellement en première année de <Text size='17px' weight="bold">Mastère 1 Développement Web</Text> chez 
-            Ynov Campus Toulouse. Je suis en parallèle en alternance à la <Text size='17px' weight="bold">Banque Populaire Caise d'Eparge (BPCE-IT)</Text>.
-        </Paragraph>
-      </Box>
-    </Grid>
-
-
-    <Grid
-    columns={{
-      count: 3,
-      size: 'auto',
-    }}
-    margin="large"
-    gap="large"
-    responsive= "true"
-    >
-      <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-        <Monitor size='xlarge' color='black' align='center' jusitfy='center'>
-        </Monitor>
-        <Text size='35px' weight="bold" textAlign="center">Développeur Web</Text>
-      </Box>
-      <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-        <Map size='xlarge' color='black' align='center' jusitfy='center'>
-        </Map>
-        <Text size='35px' weight="bold">Toulousain</Text>
-      </Box>
-      <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-        <Gamepad size='xlarge' color='black' align='center' jusitfy='center'>
-        </Gamepad>
-        <Text size='35px' weight="bold">Loisir</Text>
-      </Box>
-    </Grid>
-
-    <Box background="dark-1">
-      <Grid
-      columns={{
-        count: 3,
-        size: 'auto',
-      }}
-      
-      responsive= "true"
-      >
-        <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-          <Accessibility size='xlarge' color='white' align='center' jusitfy='center'>
-          </Accessibility>
-          <Text size='35px' weight="bold">Social</Text>
-        </Box>
-        <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-          <Bike size='xlarge' color='white' align='center' jusitfy='center'>
-          </Bike>
-          <Text size='35px' weight="bold">Sportif</Text>
-        </Box>
-        <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
-          <Edit size='xlarge' color='white' align='center' jusitfy='center'>
-          </Edit>
-          <Text size='35px' weight="bold">Créatif</Text>
-        </Box>
-      </Grid>
-    </Box>
+        {size === "small" ? (
+          <Grommet theme={customTheme}>
+          <Grid
+            rows={{
+              count: 2,
+              size: 'auto',
+            }}
+            margin="large"
+            gap="large"
+            responsive= "true"
+          >
+            <Box margin="large">
+              <Card width="medium" height="medium">
+                    <Stack anchor="bottom-left">
+                      <CardBody height="medium">
+                        <Image
+                          fit="cover"
+                          src={moi}
+                          a11yTitle="aymeric"
+                        />
+                      </CardBody>
+                      <CardHeader
+                        pad={{ horizontal: 'small', vertical: 'small' }}
+                        background="#000000A0"
+                        width="medium"
+                        justify="start"
+                      >
+                        <Avatar a11yTitle="avatar"></Avatar>
+                        <Box>
+                          <Heading level="3" margin="none">
+                            Aymeric
+                          </Heading>
+                          <Text size="small" textAlign="center" weight="bold">DELABARRE</Text>
+                        </Box>
+                      </CardHeader>
+                    </Stack>
+                  </Card>
+            </Box>
+            <Box margin="large">
+              <Paragraph>
+                <Text size='22px' weight="bold">B</Text>onjour, je m'appelle Aymeric, j'ai 21 ans et je suis 
+                  actuellement en première année de <Text size='17px' weight="bold">Mastère 1 Développement Web</Text> chez 
+                  Ynov Campus Toulouse. Je suis en parallèle en alternance à la <Text size='17px' weight="bold">Banque Populaire Caise d'Eparge (BPCE-IT)</Text>.
+              </Paragraph>
+            </Box>
+          </Grid>
     </Grommet>
+        ) : (
+        <Grommet theme={customTheme}>
+              <Grid
+                columns={{
+                  count: 2,
+                  size: 'auto',
+                }}
+                margin="large"
+                gap="large"
+                responsive= "true"
+              >
+                <Box margin="large">
+                  <Card width="medium" height="medium">
+                        <Stack anchor="bottom-left">
+                          <CardBody height="medium">
+                            <Image
+                              fit="cover"
+                              src={moi}
+                              a11yTitle="aymeric"
+                            />
+                          </CardBody>
+                          <CardHeader
+                            pad={{ horizontal: 'small', vertical: 'small' }}
+                            background="#000000A0"
+                            width="medium"
+                            justify="start"
+                          >
+                            <Avatar a11yTitle="avatar"></Avatar>
+                            <Box>
+                              <Heading level="3" margin="none">
+                                Aymeric
+                              </Heading>
+                              <Text size="small" textAlign="center" weight="bold">DELABARRE</Text>
+                            </Box>
+                          </CardHeader>
+                        </Stack>
+                      </Card>
+                </Box>
+                <Box margin="large">
+                  <Paragraph>
+                    <Text size='22px' weight="bold">B</Text>onjour, je m'appelle Aymeric, j'ai 21 ans et je suis 
+                      actuellement en première année de <Text size='17px' weight="bold">Mastère 1 Développement Web</Text> chez 
+                      Ynov Campus Toulouse. Je suis en parallèle en alternance à la <Text size='17px' weight="bold">Banque Populaire Caise d'Eparge (BPCE-IT)</Text>.
+                  </Paragraph>
+                </Box>
+              </Grid>
+        </Grommet>
+        )
+        }
+    </>
+  );
+}
+
+function GridMain() {
+  const sizeIcons = React.useContext(ResponsiveContext);
+  return (
+    <>
+      {sizeIcons === "small" ? (
+        <Grommet>
+          <Grid
+          rows={{
+            count: 3,
+            size: 'auto',
+          }}
+          margin="large"
+          gap="large"
+          responsive= "true"
+          >
+          <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+            <Monitor size='xlarge' color='black' align='center' jusitfy='center'>
+            </Monitor>
+            <Text size='35px' weight="bold" textAlign="center">Développeur Web</Text>
+          </Box>
+          <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+            <Map size='xlarge' color='black' align='center' jusitfy='center'>
+            </Map>
+            <Text size='35px' weight="bold">Toulousain</Text>
+          </Box>
+          <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+            <Gamepad size='xlarge' color='black' align='center' jusitfy='center'>
+            </Gamepad>
+            <Text size='35px' weight="bold">Loisir</Text>
+          </Box>
+        </Grid>
+    
+        <Box background="dark-1">
+          <Grid
+          rows={{
+            count: 3,
+            size: 'auto',
+          }}
+          
+          responsive= "true"
+          >
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Accessibility size='xlarge' color='white' align='center' jusitfy='center'>
+              </Accessibility>
+              <Text size='35px' weight="bold">Sociale</Text>
+            </Box>
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Bike size='xlarge' color='white' align='center' jusitfy='center'>
+              </Bike>
+              <Text size='35px' weight="bold">Sportif</Text>
+            </Box>
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Edit size='xlarge' color='white' align='center' jusitfy='center'>
+              </Edit>
+              <Text size='35px' weight="bold">Créatif</Text>
+            </Box>
+          </Grid>
+        </Box>
+      </Grommet>
+        ) : (
+        <Grommet>
+                <Grid
+            columns={{
+              count: 3,
+              size: 'auto',
+            }}
+            margin="large"
+            gap="large"
+            responsive= "true"
+            >
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Monitor size='xlarge' color='black' align='center' jusitfy='center'>
+              </Monitor>
+              <Text size='35px' weight="bold" textAlign="center">Développeur Web</Text>
+            </Box>
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Map size='xlarge' color='black' align='center' jusitfy='center'>
+              </Map>
+              <Text size='35px' weight="bold">Toulousain</Text>
+            </Box>
+            <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+              <Gamepad size='xlarge' color='black' align='center' jusitfy='center'>
+              </Gamepad>
+              <Text size='35px' weight="bold">Loisir</Text>
+            </Box>
+          </Grid>
+
+          <Box background="dark-1">
+            <Grid
+            columns={{
+              count: 3,
+              size: 'auto',
+            }}
+            
+            responsive= "true"
+            >
+              <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+                <Accessibility size='xlarge' color='white' align='center' jusitfy='center'>
+                </Accessibility>
+                <Text size='35px' weight="bold">Sociale</Text>
+              </Box>
+              <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+                <Bike size='xlarge' color='white' align='center' jusitfy='center'>
+                </Bike>
+                <Text size='35px' weight="bold">Sportif</Text>
+              </Box>
+              <Box margin="xlarge" gap="medium" align="center" animation="fadeIn">
+                <Edit size='xlarge' color='white' align='center' jusitfy='center'>
+                </Edit>
+                <Text size='35px' weight="bold">Créatif</Text>
+              </Box>
+            </Grid>
+          </Box>
+    </Grommet>
+        )
+      }
+    
     </>
     );
 }
